c4e556533a2caef92878ff7ca0b8c6a5777ab379
Tharindu <tharindu@spil.com.au>
2025-04-23 19:46:54 +0530

    Change Add Supplier And Add Bill Buttons

--- src/components/AddBillButton.jsx
+++ src/components/AddBillButton.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { FilePlus } from "lucide-react";
 import "../styles/AddBillButton.css";
 
 export default function AddBillButton({ onClick }) {
   return (
-    <button className="add-bill-btn" onClick={onClick}>
-      + Add Bill
-    </button>
+    <div className="tooltip">
+      <button className="add-bill-btn" onClick={onClick} aria-label="Add Bill">
+        <FilePlus size={20} />
+      </button>
+      <span className="tooltiptext">Add Bill</span>
+    </div>
   );
 }
--- src/components/AddSupplierButton.jsx
+++ src/components/AddSupplierButton.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Plus } from "lucide-react";
 import "../styles/AddSupplierButton.css";
 
 export default function AddSupplierButton({ onClick }) {
   return (
-    <button className="add-supplier-btn" onClick={onClick}>
-      + Add Supplier
-    </button>
+    <div className="tooltip">
+      <button
+        className="add-supplier-btn"
+        onClick={onClick}
+        aria-label="Add Supplier"
+      >
+        <Plus size={20} />
+      </button>
+      <span className="tooltiptext">Add Supplier</span>
+    </div>
   );
 }
